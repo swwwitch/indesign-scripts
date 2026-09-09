67a0f7b1a9af84e4c07b3c94beb308bf24c7e226
masahiro takano <takano@swwwitch.com>
2026-09-09 21:36:38 +0900

    no message

--- jsx/style/IdRemoveMarkerApplyStyle.jsx
+++ jsx/style/IdRemoveMarkerApplyStyle.jsx
@@ -25,8 +25,9 @@ var SCRIPT_AUTHOR   = "Masahiro Takano (@swwwitch)"; /* 作者 / author */
 var SCRIPT_RELEASED = "2026-09-09";                  /* 最初のリリース日 / first release date */
 var SCRIPT_UPDATED  = "2026-09-09";                  /* 更新日 / last updated */
 
-var SCRIPT_README_JA = "https://github.com/swwwitch/indesign-scripts/blob/main/readme-ja/IdRemoveMarkerApplyStyle.md"; /* README（日本語） */
-var SCRIPT_README_EN = "https://github.com/swwwitch/indesign-scripts/blob/main/readme-en/IdRemoveMarkerApplyStyle.md"; /* README (English) */
+var SCRIPT_README_JA   = "https://github.com/swwwitch/indesign-scripts/blob/main/readme-ja/IdRemoveMarkerApplyStyle.md"; /* README（日本語） */
+var SCRIPT_README_EN   = "https://github.com/swwwitch/indesign-scripts/blob/main/readme-en/IdRemoveMarkerApplyStyle.md"; /* README (English) */
+var SCRIPT_ARTICLE_URL = "https://note.com/dtp_tranist/n/n3a0d4c0dacdb"; /* 紹介記事 / article URL */
 
 // Released under the MIT license
 // http://opensource.org/licenses/mit-license.php
